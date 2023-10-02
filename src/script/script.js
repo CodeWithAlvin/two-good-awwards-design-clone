60c78d4db13fb6897cfe11044f925fc52e54039d
alvin saini <codewithalvin@gmail.com>
2023-10-02 12:22:26 +0530

    completed only animations need to be done

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,13 @@
 let menuIcon = document.getElementById('menu-icon')
 let menu = document.getElementById('menu')
+let logo = document.getElementById('logo')
+let nav = document.getElementsByTagName("nav")[0]
+let btnsDiv = document.getElementsByClassName("btns-div")[0]
 
 menuIcon.addEventListener("click",()=>{
 	menu.classList.toggle("opened")
+	nav.classList.toggle("color-white")
+	// logo.classList.toggle="#fff"
+	logo.classList.toggle("invert")
+	btnsDiv.classList.toggle("invert")
 })
